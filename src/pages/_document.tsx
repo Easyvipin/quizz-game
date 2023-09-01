dec7ee7a7dc7b056bb0350bd155f8837c6e52bb0
easyvipin <vipinchandra555@gmail.com>
2023-09-01 10:05:40 +0530

    feat: added meta information

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <title>Spot the Fake Game</title>
+      <Head>
+        <meta
+          name="description"
+          content="Test your skills in spotting the fake items in this exciting game."
+        />
+        <link rel="icon" href="/banned.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
